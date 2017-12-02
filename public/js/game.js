79d0fc9bcade485575ed9a740172c210a6f3d803
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2017-12-02 15:27:37 +0000

    humstar

--- public/js/game.js
+++ public/js/game.js
@@ -2,64 +2,50 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-
-    game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles-1', 'assets/tiles-1.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-    game.load.spritesheet('droid', 'assets/droid.png', 32, 32);
-    game.load.image('starSmall', 'assets/star.png');
-    game.load.image('starBig', 'assets/star2.png');
-    game.load.image('background', 'assets/background2.png');
-
+    game.load.image('stars', 'assets/starfield.jpg');
+    game.load.spritesheet('ship', 'assets/humstar.png', 32, 32);
 }
 
-var map;
-var tileset;
-var layer;
 var player;
-var facing = 'left';
-var jumpTimer = 0;
+var starfield;
 var cursors;
-var bg;
-var server;
 
 function create() {
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //  Enable P2
+    game.physics.startSystem(Phaser.Physics.P2JS);
 
-    game.stage.backgroundColor = '#000000';
+    //  Turn on impact events for the world, without this we get no collision callbacks
+    game.physics.p2.setImpactEvents(true);
 
-    bg = game.add.tileSprite(0, 0, 800, 600, 'background');
-    bg.fixedToCamera = true;
+    game.physics.p2.restitution = 0.8;
 
-    map = game.add.tilemap('level1');
+    //  Create our collision groups
+    var playerCollisionGroup = game.physics.p2.createCollisionGroup();
 
-    map.addTilesetImage('tiles-1');
+    //  This part is vital if you want the objects with their own collision groups to still collide with the world bounds
+    //  (which we do) - what this does is adjust the bounds to use its own collision group.
+    game.physics.p2.updateBoundsCollisionGroup();
 
-    map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+    starfield = game.add.tileSprite(0, 0, 800, 600, 'stars');
+    starfield.fixedToCamera = true;
 
-    layer = map.createLayer('Tile Layer 1');
+    //  Create our player sprite
+    player = game.add.sprite(200, 200, 'ship');
+    player.scale.set(2);
+    player.smoothed = false;
+    player.animations.add('fly', [0,1,2,3,4,5], 10, true);
+    player.play('fly');
 
-    //  Un-comment this on to see the collision tiles
-    // layer.debug = true;
-
-    layer.resizeWorld();
-
-    game.physics.arcade.gravity.y = 250;
-
-    player = game.add.sprite(32, 32, 'dude');
-    game.physics.enable(player, Phaser.Physics.ARCADE);
-
-    player.body.bounce.y = 0.2;
-    player.body.collideWorldBounds = true;
-    player.body.setSize(20, 32, 5, 16);
-
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('turn', [4], 20, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    game.physics.p2.enable(player, false);
+    player.body.setCircle(28);
+    player.body.fixedRotation = true;
 
     var style = { font: "12px Arial", fill: "#fff", align: "center"};
     player.text = game.add.text(0, 0, "Player 1", style);
+
+    //  Set the players collision group
+    player.body.setCollisionGroup(playerCollisionGroup);
 
     game.camera.follow(player);
 
@@ -68,8 +54,8 @@ function create() {
 
     server = {
       'player': {
-        'x': 0,
-        'y': 100,
+        'x': 200,
+        'y': 200,
       }
     }
     socket.on('position', function(x, y){
@@ -80,8 +66,9 @@ function create() {
 
 function update() {
     game.physics.arcade.moveToXY(player, server.player.x, server.player.y, 30, 30);
-    player.text.x = server.player.x;
-    player.text.y = server.player.y - player.height;
+    // game.physics.arcade.moveToXY(player.text, server.player.x, server.player.y - player.height, 30, 30);
+    player.text.x = server.player.x - player.text.width/2;
+    player.text.y = server.player.y - 48;
     //
     // game.physics.arcade.collide(player, layer);
     //
@@ -145,6 +132,7 @@ function render () {
 
 var keysdown = {
   'ArrowUp': false,
+  'ArrowDown': false,
   'ArrowLeft': false,
   'ArrowRight': false,
 };
